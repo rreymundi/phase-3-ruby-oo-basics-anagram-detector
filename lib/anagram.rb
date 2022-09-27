13cbf4a2e8b78af8a15ea2dd2f2b328f4d40296e
Raúl Reymundi <raul@kickstarter.com>
2022-09-27 17:23:53 -0400

    commit tree 0f705de09bb7eff3d9a0e192cdbd5b78e84b588e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :match
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.filter do |string|
+            string.chars.sort == @word.chars.sort
+        end
+    end
+end
